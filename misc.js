ac3d7e04f19341b4d38bdd3feaac6e76a77b12f7
soilflux <52200608+soilflux@users.noreply.github.com>
2024-12-25 05:35:14 -0500

    fix

--- misc.js
+++ misc.js
@@ -3,7 +3,7 @@ var $ = function( id ) { return document.getElementById( id ); };
 function toggleArtist() {
   if ($(event.target.id).style.opacity == '1') {
     $(event.target.id).style.opacity = '0.5';
-    selectedArtists.splice(selectedArtists.indexOf(event.target.id,1));
+    selectedArtists.splice(selectedArtists.indexOf(event.target.id),1);
   }
   else {
     $(event.target.id).style.opacity = '1';
